98dd4fbaaff9b612cd5e037c283708da8a62e195
leitto10 <46364179+leitto10@users.noreply.github.com>
2020-03-14 18:03:57 -0700

    Updates

---
+++ precache-manifest.c3a48bd40dd6caef0a66ed3a25e889ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566471e952574d5cf982529f39260555",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "388a9242bc8435193383",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08cd1ac7ef520700efd5",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388a9242bc8435193383",
+    "url": "/portfolio/static/js/main.940d9db5.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
